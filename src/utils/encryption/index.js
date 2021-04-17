afee1eeaea3a28dec391e7f9217cb66619c50f63
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-04-17 02:41:51 -0500

    fixing bugs

--- src/utils/encryption/index.js
+++ src/utils/encryption/index.js
@@ -1,9 +1,7 @@
 const bcrypt = require("bcrypt");
 
 const encryptPassword = async (password) => {
-	const hash = await bcrypt.hash(password, 5, () =>
-		console.log("Password has been encrypted successfully!"),
-	);
+	const hash = await bcrypt.hash(password, 5);
 	return hash;
 };
 
@@ -11,8 +9,6 @@ const comparePassword = async (userPassword, encryptedPassword) => {
     const isCorrect = await bcrypt.compare(userPassword, encryptedPassword)
     return isCorrect
 }
-
-
 
 module.exports = {
     encryptPassword,
