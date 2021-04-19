5484fbe0ba208ea3e8b6370b536b9e549983ace9
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-04-19 15:39:22 -0500

    response and routes done

--- src/api/lib/routes.js
+++ src/api/lib/routes.js
@@ -1,7 +1,18 @@
 const user = require('../components/Users/network')
+const product = require('../components/Products/network')
+const transaction = require('../components/Transactions/network')
+const pqr = require('../components/Pqrs/network')
+const auth = require('../components/auth/network')
+const swaggerUi = require ('swagger-ui-express');
+const swaggerDoc = require('../../public/swagger.json')
 
 const routes = server => {
-    server.use('/users', user)
+    server.use('/api/users', user)
+    server.use('/api/products', product)
+    server.use('/api/transactions', transaction)
+    server.use('/api/pqrs', pqr)
+    server.use('/api/auth', auth)
+    server.use('/api/doc', swaggerUi.serve,  swaggerUi.setup(swaggerDoc))
 }
 
 module.exports = routes
