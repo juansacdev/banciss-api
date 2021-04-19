1e0c88d49dff04384cf25936e4654fc95d1f19f3
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-04-18 21:47:47 -0500

    changes to encrypt module

--- src/utils/encryption/index.js
+++ src/utils/encryption/index.js
@@ -1,13 +1,11 @@
 const bcrypt = require("bcrypt");
 
 const encryptPassword = async (password) => {
-	const hash = await bcrypt.hash(password, 5);
-	return hash;
+	return await bcrypt.hash(password, 5);
 };
 
 const comparePassword = async (userPassword, encryptedPassword) => {
-    const isCorrect = await bcrypt.compare(userPassword, encryptedPassword)
-    return isCorrect
+    return await bcrypt.compare(userPassword, encryptedPassword)
 }
 
 module.exports = {
