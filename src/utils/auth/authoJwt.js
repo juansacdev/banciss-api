2b4c076d5f78b3379385b452fa09644eb427faed
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-04-19 01:43:38 -0500

    middleware jwt create

--- src/utils/auth/authoJwt.js
+++ src/utils/auth/authoJwt.js
@@ -1,48 +1,64 @@
-const { jwt_secret } = require('../../config');
-const response = require('../../api/lib/response')
-const jwt = require('jsonwebtoken');
+const { jwt_secret } = require("../../config");
+const response = require("../../api/lib/response");
+const jwt = require("jsonwebtoken");
+const Role = require("../../database/models/role");
+const User = require("../../database/models/user");
 
 const verifyToken = (req, res, next) => {
-    try {
-        let token = req.headers.authorization;
+	try {
+		let token = req.headers.authorization;
 
-        if (!token) {
-            return response.invalidToken({
-                res,
-                msg: 'Not token provided',
-                status: 403
-            })
-        }
+		if (!token) {
+			return response.invalidToken({
+				res,
+				msg: "Not token provided",
+				status: 403,
+			});
+		}
 
-        const decoded = jwt.verify(token, jwt_secret);
-        req.userId = decoded.id;
-        next();
-    } catch (error) {
+		const decoded = jwt.verify(token, jwt_secret);
+		req.userId = decoded.id;
+		next();
+	} catch (error) {
+		if (error.message === "jwt expired") {
+			return response.invalidToken({
+				res,
+				msg: "Your JWT has been expired",
+				status: 401,
+			});
+		}
 
-        if(error.message === "jwt expired") {
-            return response.invalidToken({
-                res,
-                msg: 'Your JWT has been expired',
-                status: 401,
-            })
-        }
+		if (error.message === "invalid signature") {
+			return response.invalidToken({
+				res,
+				msg: "Invalid Token",
+				status: 401,
+			});
+		}
 
-        if(error.message === "invalid signature") {
-            return response.invalidToken({
-                res,
-                msg: 'Invalid Token',
-                status: 401,
-            })
-        }
+		return response.invalidToken({
+			res,
+			status: 401,
+			msg: "Unauthorized",
+		});
+	}
+};
 
-        return response.invalidToken({
+const isAdmin = async (req, res, next) => {
+	const { userId } = req;
+	const user = await User.findById(userId);
+	const roleUser = await Role.findOne({ _id: { $in: user.role }});
+
+    if (roleUser.name !== 'admin') {
+        return response.isNotAdmin({
             res,
+            msg: 'User unauthorized. Require Admin role',
             status: 401,
-            msg: 'Unauthorized',
-        })
+        });
     }
-}
-
+    next()
+};
 module.exports = {
-    verifyToken,
-}
+	verifyToken,
+	isAdmin,
+};
