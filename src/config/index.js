b93be53a29fe44e2d7a76aaf2e6faa022477b224
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-04-20 06:04:55 -0500

    almost done

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@ require("dotenv").config()
 module.exports = {
 	dev: process.env.NODE_ENV !== "production",
 	port: process.env.PORT || 3000,
-	DB_URI: process.env.DB_URI || 'mongodb+srv://testUser:testUser@test.86mpl.mongodb.net/banciss-api?retryWrites=true&w=majority',
+	DB_URI: process.env.DB_URI,
 	jwt_secret: process.env.JWT_SECRET || "topSecret",
 }
